b47cbfa1caafb2d196e44b843400e928d98f00f0
iammiori <46439995+iammiori@users.noreply.github.com>
2019-03-18 03:41:56 +0900

    Iterration
    map (컴포넌트 반복시 사용)
    key 설정
    데이터 추가
    데이터 삭제 기능
    => to do list 초안

---
+++ src/Iteration.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+class Iteration extends Component{
+    state= {
+        names : ['눈사람', '얼음', '눈', '바람']
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            name : e.target.value
+        });
+    }
+
+    handleInsert = () => {
+        this.setState({
+            names : this.state.names.concat(this.state.name),
+            name : ''
+        });
+    }
+
+    handleRemove = (index) => {
+        const {names} = this.state;
+        this.setState({
+            names:[
+                ...names.slice(0,index),
+                ...names.slice(index+1,names.length)
+            ]
+        });
+    }
+    render(){
+        //const names = ['눈사람','얼음','눈','바람'];
+        const nameList = this.state.names.map(
+            (name, index)=>(<li
+                key={index}
+                onDoubleClick={()=>this.handleRemove(index)}
+            >{name}</li>)
+        );
+
+        return(
+            <div>
+                <input
+                    onChange={this.handleChange}
+                    value={this.state.name}/>
+                <button onClick={this.handleInsert}> 추가 </button>
+
+          <ul>
+              {nameList}
+          </ul>
+            </div>
+        );
+    }
+}
+
+export default Iteration;
